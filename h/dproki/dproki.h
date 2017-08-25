37918951e375e4211f1aff129cdac4fadc48d166
kasai <takara.kasai@gmail.com>
2017-08-25 10:59:53 +0900

    add root header.

---
+++ h/dproki/dproki.h
@@ -0,0 +1,16 @@
+
+#include "dproki/Sensor.h"
+#include "dproki/Common.h"
+#include "dproki/Shape.h"
+#include "dproki/ObjShape.h"
+#include "dproki/CasCoords.h"
+#include "dproki/Joint.h"
+#include "dproki/Actuator.h"
+#include "dproki/ObjSensor.h"
+#include "dproki/ObjFileReader.h"
+#include "dproki/InterfaceSceneObject.h"
+#include "dproki/DrawableLink.h"
+#include "dproki/Link.h"
+
+//dproki/dprint.h
+
